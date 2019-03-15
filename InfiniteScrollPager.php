da60c58240cb410b836a25666e51cedc3afe8a82
diazoxide <diazoxide@panteloma>
2019-03-16 02:42:56 +0400

    ...

--- InfiniteScrollPager.php
+++ InfiniteScrollPager.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace diazoxide\infinitescroll;
+
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\base\Widget;
@@ -29,28 +30,13 @@ class InfiniteScrollPager extends Widget
      * @var array HTML attributes for the link in a pager container tag.
      * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
      */
-    public $linkOptions = [];
+    public $loadButtonOptions = ['class' => 'load hidden'];
     /**
      * @var string the CSS class for the each page button.
      * @since 2.0.7
      */
     public $pageCssClass;
-    /**
-     * @var string the CSS class for the "first" page button.
-     */
-    public $firstPageCssClass = 'first';
-    /**
-     * @var string the CSS class for the "last" page button.
-     */
-    public $lastPageCssClass = 'last';
-    /**
-     * @var string the CSS class for the "previous" page button.
-     */
-    public $prevPageCssClass = 'prev';
-    /**
-     * @var string the CSS class for the "next" page button.
-     */
-    public $nextPageCssClass = 'next';
+
     /**
      * @var string the CSS class for the active (currently selected) page button.
      */
@@ -70,31 +56,11 @@ class InfiniteScrollPager extends Widget
      */
     public $disabledListItemSubTagOptions = [];
     /**
-     * @var int maximum number of page buttons that can be displayed. Defaults to 10.
-     */
-    public $maxButtonCount = 10;
-    /**
      * @var string|bool the label for the "next" page button. Note that this will NOT be HTML-encoded.
      * If this property is false, the "next" page button will not be displayed.
      */
-    public $nextPageLabel = '&raquo;';
-    /**
-     * @var string|bool the text label for the "previous" page button. Note that this will NOT be HTML-encoded.
-     * If this property is false, the "previous" page button will not be displayed.
-     */
-    public $prevPageLabel = '&laquo;';
-    /**
-     * @var string|bool the text label for the "first" page button. Note that this will NOT be HTML-encoded.
-     * If it's specified as true, page number will be used as label.
-     * Default is false that means the "first" page button will not be displayed.
-     */
-    public $firstPageLabel = false;
-    /**
-     * @var string|bool the text label for the "last" page button. Note that this will NOT be HTML-encoded.
-     * If it's specified as true, page number will be used as label.
-     * Default is false that means the "last" page button will not be displayed.
-     */
-    public $lastPageLabel = false;
+    public $loadButtonLabel = 'Load more...';
+
     /**
      * @var bool whether to register link tags in the HTML header for prev, next, first and last page.
      * Defaults to `false` to avoid conflicts when multiple pagers are used on one page.
@@ -106,11 +72,10 @@ class InfiniteScrollPager extends Widget
      * @var bool Hide widget when only one page exist.
      */
     public $hideOnSinglePage = true;
-    /**
-     * @var bool whether to render current page button as disabled.
-     * @since 2.0.12
-     */
-    public $disableCurrentPageButton = false;
+
+    public $contentSelector = null;
+
+    public $pluginOptions = [];
 
     /**
      * Initializes the pager.
@@ -120,11 +85,26 @@ class InfiniteScrollPager extends Widget
     {
         parent::init();
 
-        InfiniteScrollAsset::register($this->getView());
         if ($this->pagination === null) {
             throw new InvalidConfigException('The "pagination" property must be set.');
         }
+        InfiniteScrollAsset::register($this->getView());
+
+
     }
+
+
+    public function registerJs()
+    {
+        $this->pluginOptions['path'] = "#{$this->id}";
+        $options = json_encode($this->pluginOptions);
+        $js = <<<JS
+var elem = document.querySelector('{$this->contentSelector}');
+var infScroll = new InfiniteScroll( elem, {$options})
+JS;
+        $this->view->registerJs($js);
+    }
+
     /**
      * Executes the widget.
      * This overrides the parent implementation by displaying the generated page buttons.
@@ -135,7 +115,9 @@ class InfiniteScrollPager extends Widget
             $this->registerLinkTags();
         }
         echo $this->renderPageButtons();
+        $this->registerJs();
     }
+
     /**
      * Registers relational link tags in the html header for prev, next, first and last page.
      * These links are generated using [[\yii\data\Pagination::getLinks()]].
@@ -148,6 +130,7 @@ class InfiniteScrollPager extends Widget
             $view->registerLinkTag(['rel' => $rel, 'href' => $href], $rel);
         }
     }
+
     /**
      * Renders the page buttons.
      * @return string the rendering result
@@ -158,81 +141,17 @@ class InfiniteScrollPager extends Widget
         if ($pageCount < 2 && $this->hideOnSinglePage) {
             return '';
         }
-        $buttons = [];
         $currentPage = $this->pagination->getPage();
-        // first page
-        $firstPageLabel = $this->firstPageLabel === true ? '1' : $this->firstPageLabel;
-        if ($firstPageLabel !== false) {
-            $buttons[] = $this->renderPageButton($firstPageLabel, 0, $this->firstPageCssClass, $currentPage <= 0, false);
+
+        if (($page = $currentPage + 1) >= $pageCount - 1) {
+            $page = $pageCount - 1;
         }
-        // prev page
-        if ($this->prevPageLabel !== false) {
-            if (($page = $currentPage - 1) < 0) {
-                $page = 0;
-            }
-            $buttons[] = $this->renderPageButton($this->prevPageLabel, $page, $this->prevPageCssClass, $currentPage <= 0, false);
-        }
-        // internal pages
-        list($beginPage, $endPage) = $this->getPageRange();
-        for ($i = $beginPage; $i <= $endPage; ++$i) {
-            $buttons[] = $this->renderPageButton($i + 1, $i, null, $this->disableCurrentPageButton && $i == $currentPage, $i == $currentPage);
-        }
-        // next page
-        if ($this->nextPageLabel !== false) {
-            if (($page = $currentPage + 1) >= $pageCount - 1) {
-                $page = $pageCount - 1;
-            }
-            $buttons[] = $this->renderPageButton($this->nextPageLabel, $page, $this->nextPageCssClass, $currentPage >= $pageCount - 1, false);
-        }
-        // last page
-        $lastPageLabel = $this->lastPageLabel === true ? $pageCount : $this->lastPageLabel;
-        if ($lastPageLabel !== false) {
-            $buttons[] = $this->renderPageButton($lastPageLabel, $pageCount - 1, $this->lastPageCssClass, $currentPage >= $pageCount - 1, false);
-        }
-        $options = $this->options;
-        $tag = ArrayHelper::remove($options, 'tag', 'ul');
-        return Html::tag($tag, implode("\n", $buttons), $options);
-    }
-    /**
-     * Renders a page button.
-     * You may override this method to customize the generation of page buttons.
-     * @param string $label the text label for the button
-     * @param int $page the page number
-     * @param string $class the CSS class for the page button.
-     * @param bool $disabled whether this page button is disabled
-     * @param bool $active whether this page button is active
-     * @return string the rendering result
-     */
-    protected function renderPageButton($label, $page, $class, $disabled, $active)
-    {
-        $options = $this->linkContainerOptions;
-        $linkWrapTag = ArrayHelper::remove($options, 'tag', 'li');
-        Html::addCssClass($options, empty($class) ? $this->pageCssClass : $class);
-        if ($active) {
-            Html::addCssClass($options, $this->activePageCssClass);
-        }
-        if ($disabled) {
-            Html::addCssClass($options, $this->disabledPageCssClass);
-            $disabledItemOptions = $this->disabledListItemSubTagOptions;
-            $tag = ArrayHelper::remove($disabledItemOptions, 'tag', 'span');
-            return Html::tag($linkWrapTag, Html::tag($tag, $label, $disabledItemOptions), $options);
-        }
-        $linkOptions = $this->linkOptions;
+
+        $linkOptions = $this->loadButtonOptions;
         $linkOptions['data-page'] = $page;
-        return Html::tag($linkWrapTag, Html::a($label, $this->pagination->createUrl($page), $linkOptions), $options);
-    }
-    /**
-     * @return array the begin and end pages that need to be displayed.
-     */
-    protected function getPageRange()
-    {
-        $currentPage = $this->pagination->getPage();
-        $pageCount = $this->pagination->getPageCount();
-        $beginPage = max(0, $currentPage - (int) ($this->maxButtonCount / 2));
-        if (($endPage = $beginPage + $this->maxButtonCount - 1) >= $pageCount) {
-            $endPage = $pageCount - 1;
-            $beginPage = max(0, $endPage - $this->maxButtonCount + 1);
-        }
-        return [$beginPage, $endPage];
+        $linkOptions['id'] = $this->id;
+        $button = Html::a($this->loadButtonLabel, $this->pagination->createUrl($page), $linkOptions);
+
+        return $button;
     }
 }
